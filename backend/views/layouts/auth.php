309951c29fd87fbb1494c44c718dc7f6b6fd9331
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-07-23 11:55:41 +0200

    basic layout added

--- backend/views/layouts/auth.php
+++ backend/views/layouts/auth.php
@@ -10,9 +10,8 @@ AppAsset::register($this);
 $this->beginContent('@backend/views/layouts/base.php');
 ?>
 
-<main class="d-flex">
-    <?php echo $this->render('_sidebar'); ?>
 
+<main class="d-flex">
     <div class="content-wrapper p-3">
         <?= Alert::widget() ?>
         <?= $content ?>
